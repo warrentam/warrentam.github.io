eee64da404afadb4d4291459443daca56d03e6f5
warrentam <warrentam@gmail.com>
2021-08-05 17:58:51 +0800

    NEW

--- index.js
+++ index.js
@@ -1,88 +1,6 @@
-class Paddle {
-    constructor(gameWidth, gameHeight) {
-        this.width = 150;
-        this.height = 20;
-        this.maxSpeed = 10;
-        this.speed = 0;
-
-
-        this.position = {
-            x: gameWidth / 2 - this.width / 2,
-            y: gameHeight - this.height - 10
-
-        };
-
-
-    }
-    draw() {
-        ctx.fillStyle = '#0ff'
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-    update(deltaTime) {
-        if (!deltaTime) {
-            return;
-        } 
-        this.position.x += this.speed;  
-        if (this.position.x < 0) {
-            this.position.x = 0;
-        }     
-        if (this.position.x > 650) {
-            this.position.x = 650;
-        }
-    }
-    moveLeft() {
-        this.speed = -this.maxSpeed;
-    }
-    moveRight() {
-        this.speed = this.maxSpeed;
-    }
-    stop() {
-        this.speed = 0;
-    }
-}   
-
-class Ball {
-    constructor() {
-        this.image = document.getElementById('img_ball')
-    }
-    draw() {
-        ctx.drawImage(this.image, 10, 10, 16, 16);
-    }
-    update() {
-        
-    }
-}   
-
-class InputHandler {
-    constructor(paddle) {
-        document.addEventListener('keydown', event => {
-            switch(event.keyCode) {
-                case 37:
-                    paddle.moveLeft();
-                    break;
-                
-                case 39:
-                    paddle.moveRight();
-                    break;
-            }
-        });
-        document.addEventListener('keyup', event => {
-            switch(event.keyCode) {
-                case 37:
-                    if (paddle.speed < 0) {
-                    paddle.stop();
-                    }
-                    break;
-                
-                case 39:
-                    if (paddle.speed > 0) {
-                    paddle.stop();
-                    }
-                    break;
-            }
-        });
-    }
-}
+import InputHandler from 'inputhandler.js';
+import Ball from 'ball.js';
+import Paddle from 'paddle.js';
 let canvas = document.getElementById('gameScreen');
 let ctx = canvas.getContext('2d');
 const GAME_WIDTH = 800;
@@ -90,7 +8,7 @@ const GAME_HEIGHT = 600;
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 let ball = new Ball;
 new InputHandler(paddle);
-paddle.draw();
+paddle.draw(ctx);
 let lastTime;
 
 
@@ -99,8 +17,8 @@ function gameLoop(timeStamp) {
     lastTime = timeStamp;
     ctx.clearRect(0, 0, 800, 600);
     paddle.update(deltaTime);
-    paddle.draw();
-    ball.draw();
+    paddle.draw(ctx);
+    ball.draw(ctx);
     
     requestAnimationFrame(gameLoop);
 
