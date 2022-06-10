a8288c08a89f61bb7978a2b29d972c18e8e41479
warrentam <warrentam@gmail.com>
2022-06-10 21:39:17 +0800

    Update code.js

--- code.js
+++ code.js
@@ -4,4 +4,4 @@ var ctx = c.getContext("2d");
 var shipImg = new Image();
 shipImg.src = 'ship.png';
 const screen = new Screen(ctx, shipImg);
-shipImg.addEventListener('load', screen.drawScreen(), false);
+shipImg.addEventListener('load', screen.drawScreen());
