8229a34293b0004b0c6f9456c211b80e35909842
warrentam <warrentam@gmail.com>
2022-06-10 20:53:42 +0800

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 import Screen from '/screen.js';
 var c = document.getElementById("gameScreen");
 var ctx = c.getContext("2d");
-var img = new Image();
-img.src = 'ship.png';
-const screen = new Screen(ctx, ship);
-img.addEventListener('load', screen.drawScreen(), false);
+var shipImg = new Image();
+shipImg.src = 'ship.png';
+const screen = new Screen(ctx, shipImg);
+shipImg.addEventListener('load', screen.drawScreen(), false);
