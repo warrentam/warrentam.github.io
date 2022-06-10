38948badb9809f350992a3e7803c3d720f6dbd57
warrentam <warrentam@gmail.com>
2022-06-10 21:44:30 +0800

    wee

--- code.js
+++ code.js
@@ -1,7 +1,6 @@
 import Screen from '/screen.js';
 var c = document.getElementById("gameScreen");
 var ctx = c.getContext("2d");
-var shipImg = new Image();
-shipImg.src = 'ship.png';
+var shipImg = document.getElementByIs("ship")
 const screen = new Screen(ctx, shipImg);
 shipImg.addEventListener('load', screen.drawScreen());
